8cd9758e52e49dae55e30ca0aa3c2a6f4ab59e5f
Jehan <jehan.t@ku.th>
2025-12-04 16:15:03 +0700

    This is all I can do,Maybe is it not the right-way to do but i try my best

--- Final_exam.py
+++ Final_exam.py
@@ -14,31 +14,52 @@ class Customer(Person):
         self.item = item
 
 class Driver(Person):
-    def __init__(self, name, vechile):
+    def __init__(self, name, vechile, item, customer):
         self.vechile = vechile
-        super().__int__(name)
+        self.item = item
+        self.customer = customer
+        super().__init__(name)
 
-    def deliver(self, order):
-        print(f"{self.name} is delivering {self.item} to {self.customer} using {self.vechile}")
+    def deliver(self):
+        print(f"{self.name} is delivering {self.item} to {self.customer} using {self.vechile}.")
+
+    def Final_status(self):
+        print(f"Order for {self.item} is delivered")
 
 class DeliveryOrder:
-    def __init__(self, customer ,item , status = "delivered"):
+    def __init__(self, customer ,item , driver ,status = "delivered",):
         self.item = item
         self.customer = customer
         self.status = status
+        person = Person(driver)
+        self.driver = person.name
 
     def assign_driver(driver):
         pass
         
     def summary(self):
-        return """f
-        Item: {self.item}
-        Customer: {self.customer}
-        Status: {self.status}
-        Driver: {self.name}
-        """
-    
+        return f"Item: {self.item}\nCustomer: {self.customer}\nStatus: {self.status}\nDriver: {self.driver}"
+   
 obj1 = Person("Alice")
+obj2 = Person("Bob")
+obj3 = Person("David")
 obj1.introduce()
-Order = DeliveryOrder("Alice", "Laptop", None)
-Order.summary()
+obj2.introduce()
+obj3.introduce()
+print()
+Order1 = DeliveryOrder("Alice", "Laptop", "David", "preparing")
+print("Order Summary: ")
+print(Order1.summary())
+print()
+Order2 = DeliveryOrder("Bob", "Headphone", "Daivid", "preparing")
+print("Order Summary: ")
+print(Order2.summary())
+print()
+drv1 = Driver("David", "motorcycle", "Laptop", "Alice")
+drv2 = Driver("David", "motorcycle", "Headphones", "Bob")
+drv1.deliver()
+drv2.deliver()
+print()
+print("Final Status:")
+drv1.Final_status()
+drv2.Final_status()

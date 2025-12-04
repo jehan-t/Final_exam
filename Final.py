f123af86d82995b9599a6ce1ea83653d2e79397f
Jehan <jehan.t@ku.th>
2025-12-04 15:24:18 +0700

    thrid

--- Final.py
+++ Final.py
@@ -1,12 +1,38 @@
 class Person:
-    pass
+    def __int__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print(f"Hi, I'm {self.name}")
+
 
 class Customer(Person):
-    pass
+    def __init__(self, name, address):
+        self.address = address
+        super().__int__(name)
+
+    def place_order(self, item):
+        return DeliveryOrder()
 
 class Driver(Person):
-    pass
+    def __init__(self, name, vechile):
+        self.vechile = vechile
+        super().__int__(name)
 
-class Delivery:
-    pass
+    def deliver(self, order):
+        print(f"{self.name} is delivering {self.item} to {self.customer} using {self.vechile}")
 
+
+class DeliveryOrder:
+    def __init__(self, customer ,item , status = True):
+        self.item = item
+        self.customer = customer
+        self.status = status
+
+
+    def assign_driver(driver):
+        pass
+        
+
+    def summary(self):
+        return f"{self.customer} , {self.item}, "

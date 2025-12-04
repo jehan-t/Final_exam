b177ee66ff6d3bd2b9ac5d602f100295ba0beef0
Jehan <jehan.t@ku.th>
2025-12-04 15:47:23 +0700

    Might be the last try

--- Final_exam.py
+++ Final_exam.py
@@ -1,10 +1,9 @@
 class Person:
-    def __int__(self, name):
+    def __init__(self, name):
         self.name = name
 
     def introduce(self):
         print(f"Hi, I'm {self.name}")
-
 
 class Customer(Person):
     def __init__(self, name, address):
@@ -12,7 +11,7 @@ class Customer(Person):
         super().__int__(name)
 
     def place_order(self, item):
-        return DeliveryOrder()
+        self.item = item
 
 class Driver(Person):
     def __init__(self, name, vechile):
@@ -22,17 +21,24 @@ class Driver(Person):
     def deliver(self, order):
         print(f"{self.name} is delivering {self.item} to {self.customer} using {self.vechile}")
 
-
 class DeliveryOrder:
-    def __init__(self, customer ,item , status = True):
+    def __init__(self, customer ,item , status = "delivered"):
         self.item = item
         self.customer = customer
         self.status = status
 
-
     def assign_driver(driver):
         pass
         
-
     def summary(self):
-        return f"{self.customer} , {self.item}, "
+        return """f
+        Item: {self.item}
+        Customer: {self.customer}
+        Status: {self.status}
+        Driver: {self.name}
+        """
+    
+obj1 = Person("Alice")
+obj1.introduce()
+Order = DeliveryOrder("Alice", "Laptop", None)
+Order.summary()
